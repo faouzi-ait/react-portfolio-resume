b113533c27640ab501171e7b9c0a966e537bdaa9
Admin <admin@Admins-MacBook-Air.local>
2024-12-17 10:37:00 +0000

    Updated portfolio section

--- src/i18n/messages/fr-FR.js
+++ src/i18n/messages/fr-FR.js
@@ -1,74 +1,81 @@
-import { LOCALES } from '../constants';
+import { LOCALES } from "../constants";
 
 export default {
   [LOCALES.FRENCH]: {
     // HOME PAGE
-    subHeader: 'Développeur',
+    subHeader: "Développeur",
     subHeaderb: "4 ans d'expérience",
-    subHeader2: 'JavaScript, ReactJS & Redux',
+    subHeader2: "JavaScript, ReactJS & Redux",
 
     // SIDE NAVIGATION
-    homeLink: 'Accueil',
-    profileLink: 'Expertise',
-    experiencesLink: 'Portfolio',
-    portfolioLink: 'Experiences',
-    contactLink: 'Contacte',
-    profileTitle: 'Mon Expertise',
+    homeLink: "Accueil",
+    profileLink: "Expertise",
+    experiencesLink: "Portfolio",
+    portfolioLink: "Experiences",
+    contactLink: "Contacte",
+    profileTitle: "Mon Expertise",
 
     // MY EXPERTISE PAGE
     profileDesc: `Développeur passionné, spécialisé dans l'écosystème ReactJS et adepte de la méthodologie Agile/scrum. Intervenant à tout stade du cycle de vie de développement, de la conception au lancement en production. Confortable aussi bien dans une structure en équipe ou autonome, très orienté sur la qualité et la satisfaction de l'utilisateur.`,
 
-    librariesTitle: 'Librairies',
-    librariesDesc: 'Les librairies qui composent ma stack:',
+    librariesTitle: "Librairies",
+    librariesDesc: "Les librairies qui composent ma stack:",
     libraries:
-      'ReactJS v16+ (Hooks), useMemo, useCallback, Styled Component, React Hook Form, Responsive Design, Material-UI, React-ChartJS, React-Table.',
+      "ReactJS v16+ (Hooks), useMemo, useCallback, Styled Component, React Hook Form, Responsive Design, Material-UI, React-ChartJS, React-Table.",
 
-    statesTitle: 'Gestion des données',
+    statesTitle: "Gestion des données",
     statesDesc:
-      'Les librairies suivantes sont aussi utilisées pour gérer les données au niveau applicatif:',
-    states: 'Redux, Redux Saga, Redux Toolkit, RTK-Query, Redux Persist, Context API.',
+      "Les librairies suivantes sont aussi utilisées pour gérer les données au niveau applicatif:",
+    states:
+      "Redux, Redux Saga, Redux Toolkit, RTK-Query, Redux Persist, Context API.",
 
-    developmentTitle: 'Développement',
+    developmentTitle: "Développement",
     developmentDesc:
-      'Expérimenté dans les languages de développement ainsi que les outils suivants:',
+      "Expérimenté dans les languages de développement ainsi que les outils suivants:",
     development:
-      'JavaScript ES6+, HTML, CSS, SASS, Git, Github, Netifly, VS Code, Postman, In-Vision.',
+      "JavaScript ES6+, HTML, CSS, SASS, Git, Github, Netifly, VS Code, Postman, In-Vision.",
 
     // PORTFOLIO PAGE
-    portfolio: 'Mon Portfolio',
+    portfolio: "Mon Portfolio",
 
     // EXPERIENCE PAGE
-    experience: 'Expérience Professionnelle',
+    experience: "Expérience Professionnelle",
 
-    jobTitle1: 'Développeur Front End - React| LSEG Group | March 2024 - Present',
-    jobTitle2: 'Développeur Front End - React| NatWest Group | Juillet 2021 - Présent',
-    jobTitle3: 'Développeur Front End - React| RiskCare Financial | Octobre 2020 - Juin 2021',
+    jobTitle1:
+      "Développeur Front End - React| LSEG Group | March 2024 - Present",
+    jobTitle2:
+      "Développeur Front End - React| NatWest Group | Juillet 2021 - Présent",
+    jobTitle3:
+      "Développeur Front End - React| RiskCare Financial | Octobre 2020 - Juin 2021",
 
-    jobDesc1: "Développement d'une nouvelle plateforme de trading pour l'ensemble des services financiers du portail DMI - Capital Marketplace du groupe LSEG.",
-    jobDesc2: "Développement de plusieurs applications financières, bancaires et de dashboard au sein d'une équipe de dévevoppeur Front End, Back End et UX en proche collaboration avec le client dans une méthodologie Agile/Scrum. L'application passe d'une architecture monolithique à un modèle axé sur les services et les API. Le front-end est construit sur ReactJS v17+. Spécifiquement axé sur les hooks, composants fonctionnels, données (states) centralisées au niveau applicatif en utilisant Redux/Saga et JavaScript ES6+.",
-    jobDesc3: "Mise en œuvre de nouvelles interfaces et développement d’une application back-office de type CRM utilisée par une équipe de Finance et de Marketing pour la gestion de portefeuille clients avec contacts, messagerie, organisation de réunion, maintient de l'historique des communications avec upload de documents.",
+    jobDesc1:
+      "Développement d'une nouvelle plateforme de trading pour l'ensemble des services financiers du portail DMI - Capital Marketplace du groupe LSEG.",
+    jobDesc2:
+      "Développement de plusieurs applications financières, bancaires et de dashboard au sein d'une équipe de dévevoppeur Front End, Back End et UX en proche collaboration avec le client dans une méthodologie Agile/Scrum. L'application passe d'une architecture monolithique à un modèle axé sur les services et les API. Le front-end est construit sur ReactJS v17+. Spécifiquement axé sur les hooks, composants fonctionnels, données (states) centralisées au niveau applicatif en utilisant Redux/Saga et JavaScript ES6+.",
+    jobDesc3:
+      "Mise en œuvre de nouvelles interfaces et développement d’une application back-office de type CRM utilisée par une équipe de Finance et de Marketing pour la gestion de portefeuille clients avec contacts, messagerie, organisation de réunion, maintient de l'historique des communications avec upload de documents.",
 
     // CONTACT PAGE
-    contact: 'Contactez Moi',
-    about: 'Parlez-moi de votre project',
+    contact: "Contactez Moi",
+    about: "Parlez-moi de votre project",
 
-    collaborate: 'Et si on collaborait ensemble',
-    linkedin: 'Consulter mon profile LinkedIn',
-    github: 'Voici mes repository sur GitHub',
-    resume: 'Vous pouvez télécharger mon CV au format PDF',
+    collaborate: "Et si on collaborait ensemble",
+    linkedin: "Consulter mon profile LinkedIn",
+    github: "Voici mes repository sur GitHub",
+    resume: "Vous pouvez télécharger mon CV au format PDF",
 
-    here: 'ici',
+    here: "ici",
 
-    nameField: 'Veuillez saisir votre nom*',
-    emailField: 'Veuillez saisir votre email*',
-    messageField: 'Veuillez saisir votre message',
-    labelField: 'Parlez-moi de vous et de votre projet*',
+    nameField: "Veuillez saisir votre nom*",
+    emailField: "Veuillez saisir votre email*",
+    messageField: "Veuillez saisir votre message",
+    labelField: "Parlez-moi de vous et de votre projet*",
 
-    nameFieldError: 'Votre nom est obligatoire',
-    emailFieldError: 'Votre email est obligatoire',
-    messageFieldError: 'Votre message est obligatoire',
+    nameFieldError: "Votre nom est obligatoire",
+    emailFieldError: "Votre email est obligatoire",
+    messageFieldError: "Votre message est obligatoire",
 
-    send: 'Envoyer',
-    sending: 'Envoi en cours...',
+    send: "Envoyer",
+    sending: "Envoi en cours...",
   },
 };
--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import { t } from '../i18n/translate';
-import { DARK, LIGHT, BASE } from '../themes/index';
-import { THEMES } from '../theme';
+import { t } from "../i18n/translate";
+import { DARK, LIGHT, BASE } from "../themes/index";
+import { THEMES } from "../theme";
 
 export const getTheme = (themeName) => {
   switch (themeName) {
@@ -17,145 +17,148 @@ export const getTheme = (themeName) => {
 
 export const fullstackWork = [
   {
-    width: '30%',
-    src: 'commerce.png',
-    label: 'Fullstack Amazon E-Commerce ',
-    stack: "ReactJS, Hooks, JS ES6+, Redux/Saga, SASS, CSS Grid/Flexbox, i18n, REST, Stripe Payments, Email",
-    github: 'github.png',
-    altGithub: 'github',
-    web: 'mgn.png',
-    altWeb: 'mgn',
-    githubUrl: 'https://github.com/faouzi-ait/e-commerce-2-front',
-    webUrl: 'https://new-online-store.netlify.app',
-    showWeb: true,
-  },
-  {
-    width: '30%',
-    src: 'wave.png',
-    label: 'Fullstack E-Commerce focused on guitards',
-    stack: "ReactJS, Hooks, JS ES6+, Context API, SASS, CSS3, REST/ExpressJS, Paypal, Email",
-    github: 'github.png',
-    altGithub: 'github',
-    web: 'mgn.png',
-    altWeb: 'mgn',
-    githubUrl: 'https://github.com/faouzi-ait/waves-front',
-    webUrl: 'https://wave-store.netlify.app',
+    width: "30%",
+    src: "wave.png",
+    label: "Fullstack E-Commerce focused on guitards",
+    stack:
+    "ReactJS, Hooks, JS ES6+, Context API, SASS, CSS3, REST/ExpressJS, Paypal, Email",
+    github: "github.png",
+    altGithub: "github",
+    web: "mgn.png",
+    altWeb: "mgn",
+    githubUrl: "https://github.com/faouzi-ait/waves-front",
+    webUrl: "https://wave-store.netlify.app",
     showWeb: false,
   },
   {
-    width: '30%',
-    src: 'new-style.png',
-    label: 'Fullstack E-Commerce for men and women cloting',
-    stack: "ReactJS, Hooks, JS ES6+, Redux/Saga, Responsive Design, CSS3, CSS Grid/Flexbox, REST/ExpressJS, Paypal, Email",
-    github: 'github.png',
-    altGithub: 'github',
-    web: 'mgn.png',
-    altWeb: 'mgn',
-    githubUrl: 'https://github.com/faouzi-ait/e-commerce-front',
-    webUrl: 'https://new-style.netlify.app',
+    width: "30%",
+    src: "commerce.png",
+    label: "Fullstack Amazon E-Commerce ",
+    stack:
+      "ReactJS, Hooks, JS ES6+, Redux/Saga, SASS, CSS Grid/Flexbox, i18n, REST, Stripe Payments, Email",
+    github: "github.png",
+    altGithub: "github",
+    web: "mgn.png",
+    altWeb: "mgn",
+    githubUrl: "https://github.com/faouzi-ait/e-commerce-2-front",
+    webUrl: "https://new-online-store.netlify.app",
+    showWeb: true,
+  },
+  {
+    width: "30%",
+    src: "new-style.png",
+    label: "Fullstack E-Commerce for men and women cloting",
+    stack:
+      "ReactJS, Hooks, JS ES6+, Redux/Saga, Responsive Design, CSS3, CSS Grid/Flexbox, REST/ExpressJS, Paypal, Email",
+    github: "github.png",
+    altGithub: "github",
+    web: "mgn.png",
+    altWeb: "mgn",
+    githubUrl: "https://github.com/faouzi-ait/e-commerce-front",
+    webUrl: "https://new-style.netlify.app",
     showWeb: false,
   },
 ];
 
 export const uiWork = [
   {
-    width: '30%',
-    src: 'nexter.png',
-    label: 'Real estate presentation user interface',
+    width: "30%",
+    src: "nexter.png",
+    label: "Real estate presentation user interface",
 
-    github: 'github.png',
-    altGithub: 'github',
-    web: 'mgn.png',
-    altWeb: 'mgn',
-    githubUrl: 'https://github.com/faouzi-ait/Nexter',
-    webUrl: 'https://real-estates-agency.netlify.app',
+    github: "github.png",
+    altGithub: "github",
+    web: "mgn.png",
+    altWeb: "mgn",
+    githubUrl: "https://github.com/faouzi-ait/Nexter",
+    webUrl: "https://real-estates-agency.netlify.app",
     showWeb: false,
   },
   {
-    width: '30%',
-    src: 'map.png',
-    label: 'Building filtering map in Zurich, Switzerland',
+    width: "30%",
+    src: "instagram.png",
+    label: "Fullstack & fully featured Instagram website",
 
-    github: 'github.png',
-    altGithub: 'github',
-    web: 'mgn.png',
-    altWeb: 'mgn',
-    githubUrl: 'https://github.com/faouzi-ait/nomoko',
-    webUrl: 'https://nomoko-map.netlify.app',
-    showWeb: false,
+    github: "github.png",
+    altGithub: "github",
+    web: "mgn.png",
+    altWeb: "mgn",
+    githubUrl: "https://github.com/faouzi-ait/instagram-web",
+    webUrl: "https://insta-web.netlify.app/",
+    showWeb: true,
   },
   {
-    width: '30%',
-    src: 'trillio.png',
-    label: 'Hotel resort website for holiday makers',
+    width: "30%",
+    src: "trillio.png",
+    label: "Hotel resort website for holiday makers",
 
-    github: 'github.png',
-    altGithub: 'github',
-    web: 'mgn.png',
-    altWeb: 'mgn',
-    githubUrl: 'https://github.com/faouzi-ait/Trillo',
-    webUrl: 'https://trillo-reservation.netlify.app',
+    github: "github.png",
+    altGithub: "github",
+    web: "mgn.png",
+    altWeb: "mgn",
+    githubUrl: "https://github.com/faouzi-ait/Trillo",
+    webUrl: "https://trillo-reservation.netlify.app",
     showWeb: false,
   },
 ];
 
 export const accordionData = [
   {
-    title: t('jobTitle1'),
-    content: t('jobDesc1'),
+    title: t("jobTitle1"),
+    content: t("jobDesc1"),
     env: [
-      'NX',
-      'NextJS',
-      'TypeScript',
-      'JavaScript ES6+',
-      'Forge-XD',
-      'Material-UI',
-      'Context API',
-      'REST API',
-      'HTML',
-      'CSS',
-      'SASS',
+      "NX",
+      "NextJS",
+      "TypeScript",
+      "JavaScript ES6+",
+      "Forge-XD",
+      "Material-UI",
+      "Context API",
+      "REST API",
+      "HTML",
+      "CSS",
+      "SASS",
     ],
   },
   {
-    title: t('jobTitle2'),
-    content: t('jobDesc2'),
+    title: t("jobTitle2"),
+    content: t("jobDesc2"),
     env: [
-      'React v17+',
-      'React Hooks',
-      'useMemo',
-      'useCallback',
-      'Redux',
-      'Redux Saga',
-      'Redux Persist',
-      'JavaScript ES6+',
-      'HTML',
-      'CSS',
-      'SASS',
-      'Styled Component',
-      'React-Table',
-      'React-Chart-JS',
-      'TreeMap',
-      'Material-UI',
-      'REST API',
+      "React v17+",
+      "React Hooks",
+      "useMemo",
+      "useCallback",
+      "Redux",
+      "Redux Saga",
+      "Redux Persist",
+      "JavaScript ES6+",
+      "HTML",
+      "CSS",
+      "SASS",
+      "Styled Component",
+      "React-Table",
+      "React-Chart-JS",
+      "TreeMap",
+      "Material-UI",
+      "REST API",
     ],
   },
   {
-    title: t('jobTitle3'),
-    content: t('jobDesc3'),
+    title: t("jobTitle3"),
+    content: t("jobDesc3"),
     env: [
-      'React v17+',
-      'React Hooks',
-      'Redux',
-      'Redux Saga',
-      'Redux',
-      'JavaScript ES6+',
-      'HTML',
-      'Git/GitHub',
-      'CSS',
-      'SASS',
-      'Styled Component',
-      'REST API.',
+      "React v17+",
+      "React Hooks",
+      "Redux",
+      "Redux Saga",
+      "Redux",
+      "JavaScript ES6+",
+      "HTML",
+      "Git/GitHub",
+      "CSS",
+      "SASS",
+      "Styled Component",
+      "REST API.",
     ],
   },
 ];
@@ -174,11 +177,11 @@ export const createField = (
         type={type}
         name={name}
         placeholder={placeHolder}
-        className={errors?.name ? 'error' : ''}
+        className={errors?.name ? "error" : ""}
         autoComplete="off"
         ref={register({ required: true })}
       />
-      {errors?.name && errors?.name?.type === 'required' && (
+      {errors?.name && errors?.name?.type === "required" && (
         <p className="login__error">{errorLabel}</p>
       )}
     </div>
@@ -187,6 +190,6 @@ export const createField = (
 
 export const resetMessage = (setError) => {
   setTimeout(() => {
-    setError('');
+    setError("");
   }, 4000);
 };

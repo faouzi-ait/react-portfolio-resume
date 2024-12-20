6cf3717603b39a7fd1ad9e33d3f0e0cf4f9f9398
Admin <admin@Admins-MacBook-Air.local>
2024-12-20 14:33:35 +0000

    Portfolio update

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -17,11 +17,11 @@ export const getTheme = (themeName) => {
 
 export const fullstackWork = [
   {
-    width: "30%",
+    width: "45%",
     src: "wave.png",
     label: "Fullstack E-Commerce focused on guitards",
     stack:
-    "ReactJS, Hooks, JS ES6+, Context API, SASS, CSS3, REST/ExpressJS, Paypal, Email",
+      "ReactJS, Hooks, JS ES6+, Context API, SASS, CSS3, REST/ExpressJS, Paypal, Email",
     github: "github.png",
     altGithub: "github",
     web: "mgn.png",
@@ -30,8 +30,78 @@ export const fullstackWork = [
     webUrl: "https://wave-store.netlify.app",
     showWeb: false,
   },
+  // {
+  //   width: "30%",
+  //   src: "commerce.png",
+  //   label: "Fullstack Amazon E-Commerce ",
+  //   stack:
+  //     "ReactJS, Hooks, JS ES6+, Redux/Saga, SASS, CSS Grid/Flexbox, i18n, REST, Stripe Payments, Email",
+  //   github: "github.png",
+  //   altGithub: "github",
+  //   web: "mgn.png",
+  //   altWeb: "mgn",
+  //   githubUrl: "https://github.com/faouzi-ait/e-commerce-2-front",
+  //   webUrl: "https://new-online-store.netlify.app",
+  //   showWeb: true,
+  // },
   {
-    width: "30%",
+    width: "45%",
+    src: "instagram.png",
+    label: "Fullstack & fully featured Instagram website",
+
+    github: "github.png",
+    altGithub: "github",
+    web: "mgn.png",
+    altWeb: "mgn",
+    githubUrl: "https://github.com/faouzi-ait/instagram-web",
+    webUrl: "https://insta-web.netlify.app/",
+    showWeb: true,
+  },
+  // {
+  //   width: "30%",
+  //   src: "new-style.png",
+  //   label: "Fullstack E-Commerce for men and women cloting",
+  //   stack:
+  //     "ReactJS, Hooks, JS ES6+, Redux/Saga, Responsive Design, CSS3, CSS Grid/Flexbox, REST/ExpressJS, Paypal, Email",
+  //   github: "github.png",
+  //   altGithub: "github",
+  //   web: "mgn.png",
+  //   altWeb: "mgn",
+  //   githubUrl: "https://github.com/faouzi-ait/e-commerce-front",
+  //   webUrl: "https://new-style.netlify.app",
+  //   showWeb: false,
+  // },
+];
+
+export const uiWork = [
+  // {
+  //   width: "30%",
+  //   src: "nexter.png",
+  //   label: "Real estate presentation user interface",
+
+  //   github: "github.png",
+  //   altGithub: "github",
+  //   web: "mgn.png",
+  //   altWeb: "mgn",
+  //   githubUrl: "https://github.com/faouzi-ait/Nexter",
+  //   webUrl: "https://real-estates-agency.netlify.app",
+  //   showWeb: false,
+  // },
+  // {
+  //   width: "30%",
+  //   src: "instagram.png",
+  //   label: "Fullstack & fully featured Instagram website",
+
+  //   github: "github.png",
+  //   altGithub: "github",
+  //   web: "mgn.png",
+  //   altWeb: "mgn",
+  //   githubUrl: "https://github.com/faouzi-ait/instagram-web",
+  //   webUrl: "https://insta-web.netlify.app/",
+  //   showWeb: true,
+  // },
+  {
+    width: "60%",
     src: "commerce.png",
     label: "Fullstack Amazon E-Commerce ",
     stack:
@@ -44,62 +114,19 @@ export const fullstackWork = [
     webUrl: "https://new-online-store.netlify.app",
     showWeb: true,
   },
-  {
-    width: "30%",
-    src: "new-style.png",
-    label: "Fullstack E-Commerce for men and women cloting",
-    stack:
-      "ReactJS, Hooks, JS ES6+, Redux/Saga, Responsive Design, CSS3, CSS Grid/Flexbox, REST/ExpressJS, Paypal, Email",
-    github: "github.png",
-    altGithub: "github",
-    web: "mgn.png",
-    altWeb: "mgn",
-    githubUrl: "https://github.com/faouzi-ait/e-commerce-front",
-    webUrl: "https://new-style.netlify.app",
-    showWeb: false,
-  },
-];
+  // {
+  //   width: "30%",
+  //   src: "trillio.png",
+  //   label: "Hotel resort website for holiday makers",
 
-export const uiWork = [
-  {
-    width: "30%",
-    src: "nexter.png",
-    label: "Real estate presentation user interface",
-
-    github: "github.png",
-    altGithub: "github",
-    web: "mgn.png",
-    altWeb: "mgn",
-    githubUrl: "https://github.com/faouzi-ait/Nexter",
-    webUrl: "https://real-estates-agency.netlify.app",
-    showWeb: false,
-  },
-  {
-    width: "30%",
-    src: "instagram.png",
-    label: "Fullstack & fully featured Instagram website",
-
-    github: "github.png",
-    altGithub: "github",
-    web: "mgn.png",
-    altWeb: "mgn",
-    githubUrl: "https://github.com/faouzi-ait/instagram-web",
-    webUrl: "https://insta-web.netlify.app/",
-    showWeb: true,
-  },
-  {
-    width: "30%",
-    src: "trillio.png",
-    label: "Hotel resort website for holiday makers",
-
-    github: "github.png",
-    altGithub: "github",
-    web: "mgn.png",
-    altWeb: "mgn",
-    githubUrl: "https://github.com/faouzi-ait/Trillo",
-    webUrl: "https://trillo-reservation.netlify.app",
-    showWeb: false,
-  },
+  //   github: "github.png",
+  //   altGithub: "github",
+  //   web: "mgn.png",
+  //   altWeb: "mgn",
+  //   githubUrl: "https://github.com/faouzi-ait/Trillo",
+  //   webUrl: "https://trillo-reservation.netlify.app",
+  //   showWeb: false,
+  // },
 ];
 
 export const accordionData = [

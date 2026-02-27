4ff3d557217aa4a1ed17d5da7beb94a2c33ad2ba
Admin <admin@Admins-MacBook-Air.local>
2026-02-27 12:14:24 +0100

    Updated resume

--- src/i18n/messages/en-US.js
+++ src/i18n/messages/en-US.js
@@ -1,19 +1,19 @@
-import { LOCALES } from '../constants';
+import { LOCALES } from "../constants";
 
 const enUS = {
   [LOCALES.ENGLISH]: {
     // HOME PAGE
-    subHeader: 'Developer',
-    subHeaderb: '4+ years of experience',
-    subHeader2: 'JavaScript, ReactJS & Redux',
+    subHeader: "Developer",
+    subHeaderb: "4+ years of experience",
+    subHeader2: "JavaScript, ReactJS & Redux",
 
     // SIDE NAVIGATION
-    homeLink: 'Home',
-    profileLink: 'Expertise',
-    experiencesLink: 'Portfolio',
-    portfolioLink: 'Experiences',
-    contactLink: 'Contact me',
-    profileTitle: 'My Expertise',
+    homeLink: "Home",
+    profileLink: "Expertise",
+    experiencesLink: "Portfolio",
+    portfolioLink: "Experiences",
+    contactLink: "Contact me",
+    profileTitle: "My Expertise",
 
     // MY EXPERTISE PAGE
     profileDesc: `Passionate developer, specialized in the ReactJS ecosystem
@@ -22,58 +22,65 @@ const enUS = {
           Comfortable within a feature team or in an autonomous structure.
           Result and quality oriented, always experimenting and reviewing the latest trends and evolutions.`,
 
-    librariesTitle: 'Libraries',
-    librariesDesc: 'Experience developping applications using:',
+    librariesTitle: "Libraries",
+    librariesDesc: "Experience developping applications using:",
     libraries:
-      'ReactJS v16+ (Hooks), useMemo, useCallback, Styled Component, React Hook Form, Responsive Design, Material-UI, React-ChartJS, React-Table.',
+      "ReactJS v16+ (Hooks), useMemo, useCallback, Styled Component, React Hook Form, Responsive Design, Material-UI, React-ChartJS, React-Table.",
 
-    statesTitle: 'State Management',
+    statesTitle: "State Management",
     statesDesc:
-      'The following state managements solution are used extensively with ReactJS:',
-    states: 'Redux, Redux Saga, Redux Toolkit, RTK-Query, Redux Persist, Context API.',
+      "The following state managements solution are used extensively with ReactJS:",
+    states:
+      "Redux, Redux Saga, Redux Toolkit, RTK-Query, Redux Persist, Context API.",
 
-    developmentTitle: 'Development',
+    developmentTitle: "Development",
     developmentDesc:
-      'Experienced in front end development using the following core technologies:',
+      "Experienced in front end development using the following core technologies:",
     development:
-      'JavaScript ES6+, HTML, CSS, SASS, Git, Github, Netifly, VS Code, Postman, In-Vision.',
+      "JavaScript ES6+, HTML, CSS, SASS, Git, Github, Netifly, VS Code, Postman, In-Vision.",
 
     // PORTFOLIO PAGE
-    portfolio: 'My Portfolio',
+    portfolio: "My Portfolio",
 
     // EXPERIENCE PAGE
-    experience: 'Professional Experiences',
+    experience: "Professional Experiences",
 
-    jobTitle1: 'Front End - React Developer | LSEG Group | March 2024 - Present',
-    jobTitle2: 'Front End - React Developer | NatWest Group | July 2021 - August 2023',
-    jobTitle3: 'Front End - React Developer | RiskCare Financial | October 2020 - June 2021',
+    jobTitle1:
+      "Front End - React Developer | LSEG Group | March 2024 - Present",
+    jobTitle2:
+      "Front End - React Developer | NatWest Group | July 2021 - August 2023",
+    jobTitle3:
+      "Front End - React Developer | RiskCare Financial | October 2020 - June 2021",
 
-    jobDesc1: 'Currently working in developing and enhancing the front-end trading solutions for the DMI Marketplace financial service portal for the London Stock Exchange Group',
-    jobDesc2: 'developing the Financials, Banking and Dashboard applications along with the migration from the current monolithic architecture to a service oriented, API driven model. The front-end is built using ReactJS v17+. Specifically focused on Hooks, Functional Components, centralized data using Redux/Saga and JavaScript ES6+.',
-    jobDesc3: 'Implementation of new interfaces and further development of a back-office application of type CRM used by a Finance and Marketing team to manage a portfolio of clients with contacts, messages, meeting requests, communication history and documentation submission.',
+    jobDesc1:
+      "Currently working in developing and enhancing the front-end trading solutions for the DMI Marketplace financial service portal for the London Stock Exchange Group",
+    jobDesc2:
+      "developing the Financials, Banking and Dashboard applications along with the migration from the current monolithic architecture to a service oriented, API driven model. The front-end is built using ReactJS v17+. Specifically focused on Hooks, Functional Components, centralized data using Redux/Saga and JavaScript ES6+.",
+    jobDesc3:
+      "Implementation of new interfaces and further development of a back-office application of type CRM used by a Finance and Marketing team to manage a portfolio of clients with contacts, messages, meeting requests, communication history and documentation submission.",
 
     // CONTACT PAGE
-    contact: 'Contact Me',
-    about: 'Tell me about your project',
+    contact: "Contact Me",
+    about: "Tell me about your project",
 
     collaborate: "Let's build something together",
-    linkedin: 'Take a look at my LinkedIn profile',
-    github: 'Checkout my GitHub repositories',
-    resume: 'You can download my resume in a PDF format',
+    linkedin: "Take a look at my LinkedIn profile",
+    github: "Checkout my GitHub repositories",
+    resume: "You can download my resume in a PDF format",
 
-    here: 'ici',
+    here: "ici",
 
-    nameField: 'Please enter your full name*',
-    emailField: 'Please enter your email*',
-    messageField: 'Please enter your message',
-    labelField: 'Tell me about you and your project*',
+    nameField: "Please enter your full name*",
+    emailField: "Please enter your email*",
+    messageField: "Please enter your message",
+    labelField: "Tell me about you and your project*",
 
-    nameFieldError: 'Your name is mandatory',
-    emailFieldError: 'Your email is mandatory',
-    messageFieldError: 'Your message is mandatory',
+    nameFieldError: "Your name is mandatory",
+    emailFieldError: "Your email is mandatory",
+    messageFieldError: "Your message is mandatory",
 
-    send: 'Send',
-    sending: 'Sending your message...',
+    send: "Send",
+    sending: "Sending your message...",
   },
 };
 
